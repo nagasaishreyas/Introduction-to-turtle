e7b3d642717d7170858097dfee3e8cf4680120f3
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-08-13 20:58:02 +0530

    square

---
+++ main4.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(90)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
+
+board.left(90)
+board.forward(100)
