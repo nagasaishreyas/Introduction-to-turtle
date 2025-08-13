25adc2ddc7946148feaca515e43baa1895246bcf
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-08-13 20:11:14 +0530

    mir pir

---
+++ main.py
@@ -0,0 +1,12 @@
+row = 5
+for i in range(1, row +1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+    
+
+row = 5
+for i in range(row, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
